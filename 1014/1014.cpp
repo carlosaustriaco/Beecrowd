26ae468176a5ed68f4baf153c3e0cd5947752edc
Carlos <carlos.aaustriaco@gmail.com>
2026-02-05 15:02:56 -0300

    1014

---
+++ 1014/1014.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int k;
+    double l;
+
+    cin >> k >> l;
+
+    cout << fixed;
+    cout << setprecision(3);
+    cout << k/l << " km/l" << endl;
+
+    return 0;
+}
