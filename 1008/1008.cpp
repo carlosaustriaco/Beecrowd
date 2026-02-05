d52f10018742892018c04f63baa9d5432456cb29
Carlos <carlos.aaustriaco@gmail.com>
2026-02-05 14:14:38 -0300

    1008

---
+++ 1008/1008.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, h;
+    double v;
+
+    cin >> n >> h >> v;
+
+    cout << "NUMBER = " << n << endl;
+    cout << fixed;
+    cout << setprecision(2);
+    cout << "SALARY = U$ " << h*v << endl;
+
+    return 0;
+}
