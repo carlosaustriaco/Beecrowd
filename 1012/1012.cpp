64b24cdee8873cf7c8db71ca23696565a3fc25e8
Carlos <carlos.aaustriaco@gmail.com>
2026-02-05 15:12:51 -0300

    1012

---
+++ 1012/1012.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    double a, b, c;
+
+    cin >> a >> b >> c;
+    cout << fixed;
+    cout << setprecision(3);
+    cout << "TRIANGULO: " << a*c/2 << endl;
+    cout << "CIRCULO: " << 3.14159*c*c << endl;
+    cout << "TRAPEZIO: " << (a+b)*c/2 << endl;
+    cout << "QUADRADO: " << b*b << endl;
+    cout << "RETANGULO: " << a*b << endl;
+
+    return 0;
+}
