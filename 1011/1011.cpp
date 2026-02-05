fc3a87d28c4bc949daee7028300b5e214d6d7192
Carlos <carlos.aaustriaco@gmail.com>
2026-02-05 14:59:22 -0300

    1011

---
+++ 1011/1011.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    double r;
+    const double pi = 3.14159;
+
+    cin >> r;
+    cout << fixed;
+    cout << setprecision(3);
+    cout << "VOLUME = " << pi*4*r*r*r/3 << endl;
+    return 0;
+}
