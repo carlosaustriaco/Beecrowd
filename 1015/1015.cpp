5a7fec34ba476a8f7c81698f3a6a3c5afd5bc86c
Carlos <carlos.aaustriaco@gmail.com>
2026-02-05 15:23:42 -0300

    1015

---
+++ 1015/1015.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    double x1,x2,y1,y2;
+
+    cin >> x1 >> y1 >> x2 >> y2;
+    cout << fixed;
+    cout << setprecision(4);
+    cout << sqrt(pow(x1-x2,2)+pow(y1-y2,2)) << endl;
+
+    return 0;
+}
