adcdcf9a732a75c466f62630e1d6a6d28c3e0bd1
Carlos <carlos.aaustriaco@gmail.com>
2026-02-06 19:37:26 -0300

    1043

---
+++ 1043/1043.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    double a,b,c;
+
+    cin >> a >>b >> c;
+
+    cout << fixed;
+    cout<<setprecision(1);
+    if((a<(b+c)) && (b<(a+c))&&(c<(a+b))){
+        cout << "Perimetro = " << a+b+c<<endl;
+    }
+    else{
+        cout <<"Area = "<<(a+b)*c/2<<endl;
+    }
+
+    return 0;
+}
