692e117e9d3d4233972de4cbbb57b91cf4a5ed84
Carlos <carlos.aaustriaco@gmail.com>
2026-02-09 14:49:59 -0300

    1103

---
+++ 1103/1103.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int h1,h2,m1,m2,r;
+
+    while(true){
+        cin >> h1 >> m1 >> h2 >> m2;
+
+        if((h1==0)&&(h2==0)&&(m1==0)&&(m2==0))
+            break;
+
+        if(((h2==h1)&&(m2<m1))||(h2<h1)){
+            h2+=24;
+        }
+
+        cout << (h2*60+m2)-(h1*60+m1) << endl;
+    }
+
+    return 0;
+}
